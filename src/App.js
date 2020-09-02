3d8bcce0397826a0044914b8b8b1e9aeb0186162
Mathew Varghese <mathewmvv@icloud.com>
2020-09-02 12:59:42 +1200

    implemented graphql

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
 import './App.css';
 
+
+const COUNTRY_DETAILS = gql`{
+  countryDetails(name: "India") {
+    name,
+    capital
+  }
+}`
 function App() {
+  const { loading, error, data } = useQuery(COUNTRY_DETAILS);
+  if(loading) return <h1>Loading</h1>
+  if(error) return <h1>Error</h1>
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Country Details...</h2>
+  <h2>{ data.countryDetails.map((country, id) => 
+        <div>
+          <h1 key = { id }>{ country.name }</h1>
+          <p>{ country.capital }</p>
+        </div>
+      )}
+  </h2>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3000/graphql'
+  })
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client= { client }>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
